ac9222409dea5a8a547e30995f7133ca473921a0
Clint Bush <clint@madebyclint.com>
2024-10-02 19:39:02 -0700

    Initial Commit

---
+++ SimpleGroceryList/SimpleGroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleGroceryListApp.swift
+//  SimpleGroceryList
+//
+//  Created by Clint Bush on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleGroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleGroceryListTests/SimpleGroceryListTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleGroceryListTests.swift
+//  SimpleGroceryListTests
+//
+//  Created by Clint Bush on 10/2/24.
+//
+
+import Testing
+
+struct SimpleGroceryListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
