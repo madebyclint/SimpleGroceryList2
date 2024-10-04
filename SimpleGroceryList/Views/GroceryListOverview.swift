d6a7ffbe3f77c74e9b49de300062226b8ba91a57
Clint Bush <clint@madebyclint.com>
2024-10-03 20:14:18 -0700

    WIP trying to get save to work properly

---
+++ SimpleGroceryList/Views/GroceryListOverview.swift
@@ -0,0 +1,18 @@
+//
+//  GroceryListOverview.swift
+//  SimpleGroceryList
+//
+//  Created by Clint Bush on 10/3/24.
+//
+
+import SwiftUI
+
+struct GroceryListOverview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroceryListOverview()
+}
